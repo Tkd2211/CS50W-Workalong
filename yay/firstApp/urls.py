55e3bb5c8b9bae84ae3821bbe5b005a645abed96
Tejas Dubey <123975589+Tkd2211@users.noreply.github.com>
2026-03-17 14:45:39 +0530

    fresh start

--- yay/firstApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("scale", views.scale, name="scale"),
-    path("<str:name>", views.greet, name="greet")
-]
